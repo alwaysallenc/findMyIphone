2bc86c90e8c708cd48b65fd2ec96cf2b579ca567
alwaysallenc <allen.childers5@gmail.com>
2018-10-17 19:43:50 -0500

    final update

--- js/index.js
+++ js/index.js
@@ -1,34 +1,37 @@
 //use jquery
 
 $(document).ready(function(){
+
   var username = $('input[type=text]');
   var password = $('input[type=password]');
+
   button = $("button");
+
   var usernameTxt = $('input[type=text]').val();
   var passwordTxt = $('input[type=password]').val();
+
   toggleButton();
+
   username
     .keypress(toggleButton)
     .keyup(toggleButton);
+
   password
     .keypress(toggleButton)
     .keyup(toggleButton);
-  
+
   function toggleButton() {
     usrLen = username.val().length;
     pwdLen = password.val().length;
     if (usrLen != 0 && pwdLen != 0) {
      button
        .removeAttr("disabled")
-       .addClass('activeButton'); 
-    } 
+       .addClass('activeButton');
+    }
     else {
        button.attr("disabled", "disabled");
-       button.removeClass('activeButton'); 
+       button.removeClass('activeButton');
     }
   };
   
-  // if(usernameTxt!="" && passwordTxt!=""){
-  //   document.getElementById("signIn").disabled = false;
-  // };
 });
